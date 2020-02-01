02cdc7d4016e319ff24debfdced646f5dc0eecb7
Neetish Kumar <neetish3b@gmail.com>
2020-02-01 19:29:10 +0530

    sidebar added

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,96 +1,102 @@
-import React from 'react';
-import './sidebar.css'
+import React from "react";
+import "./sidebar.css";
 
-class Sidebar extends React.Component  {
-  render(){
+class Sidebar extends React.Component {
+  state = {
+    isSideBarVisible:false,
+    isFlylistShown:false
+  };
+  toggleBox = () => {
+    this.setState(prevState => ({ isSideBarVisible: !prevState.isSideBarVisible }));
+  };
+  render() {
+    const { isSideBarVisible } = this.state;
     return (
-      <div className="doNotPrint">
-        <div className="dashboar">
-          <div className="leftMenuBar1">
-            <div className="logo text-center">
-              <img className="mt10" src="../assets/images/Group.png" />
+      <div className="dashboar">
+        <div className="leftMenuBar1">
+          <div className="logo text-center">
+            <img src={require("../assets/images/Group.png")} alt="logo" />
+          </div>
+          <div class="menue text-center">
+            <a>
+              <span className="fIcon icon-list ifit" onClick={this.toggleBox}></span>
+            </a>
+          </div>
+          <div class="bst">
+            <div class="avatar">
+              <h4 class="avatar-text">NK</h4>
             </div>
-            <div className="menue text-center">
-              <a href="javascript:void(0)"> <span className="fIcon icon-list ifit"></span></a>
-            </div>
-            <div className="bst">
-              <div class="menue text-center">
-                <a href="javascript:void(0)"><span class="fIcon icon-settings32 ifit"></span></a>
-              </div>
-              <div class="menue text-center">
-                <a href="javascript:void(0)"><span class="fIcon icon-notifications2 ifit"></span></a>
-              </div>
-              <div class="menue text-center">
-                <a href="javascript:void(0)">
-                  <img class="help" src="../assets/images/help.png" />
+            <div className="flylist">
+              <div className="flylist-link">
+                <a className="">
+                  <i class="custom-icon icon-user pr10"></i>Profile
+                </a>
+                <a class="" href="javascript:void(0)">
+                  <i class="custom-icon icon-sign-out pr10"></i>Logout
                 </a>
               </div>
-              <div class="avatar">
-                <h4 class="avatar-text">test</h4>
-              </div>
-              <div className="flylist">
-                <div className="flylist-link">
-                  <a className="">
-                    <i className="custom-icon icon-user pr10"></i>Profile</a>
-                  <a className="" href="javascript:void(0)">
-                    <i className="custom-icon icon-sign-out pr10"></i>Logout</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div id="1" className="">
-            <div className="form-group">
-              <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Search" />
-              <h3 className="title">Sygnio Sections</h3>
-              <div class="sideMenueWidgit text-center bg-orange">
-                <img class="mb10" src="../assets/images/dashboard.png" />
-                <i class="custom-icon icon-menu"></i>
-                <p>Dashboard</p>
-              </div>
-              <div className="sideMenueWidgit text-center bg-yellow">
-                <img className="mb10" src="../assets/images/Staff_management.png" />
-                <i class="custom-icon icon-menu"></i>
-                <p>Staff Management</p>
-              </div>
-              <div className="sideMenueWidgit text-center bg-floor">
-                <i className="custom-icon icon-floor"></i>
-                <p>Floor Management</p>
-              </div>
-              <div className="sideMenueWidgit text-center bg-green">
-                <img className="mb10" src="../assets/images/Inventory_management.png" />
-                <i className="custom-icon icon-menu"></i>
-                <p>Inventory Management</p>
-              </div>
-              <div className="sideMenueWidgit text-center bg-pos">
-                <i className="custom-icon icon-floor"></i>
-                <p>POS</p>
-              </div>
-              <div className="sideMenueWidgit text-center bg-transaction">
-                <i className="custom-icon icon-floor"></i>
-                <p>Transactions</p>
-              </div>
-              <div className="sideMenueWidgit text-center bg-report">
-                <i className="custom-icon icon-floor"></i>
-                <p>Reports</p>
-              </div>
-              <div class="sideMenueWidgit text-center bg-customer">
-                <i class="custom-icon icon-floor"></i>
-                <p>Customers</p>
-              </div>
-              <div class="sideMenueWidgit text-center bg-setting">
-                <i class="custom-icon icon-floor"></i>
-                <p>Settings</p>
-              </div>
-              <div class="sideMenueWidgit text-center bg-orders" >
-                <i class="custom-icon icon-floor"></i>
-                <p>Orders</p>
-              </div>
-
             </div>
           </div>
         </div>
+        <div className={`sideMenue ${isSideBarVisible ? "": "open"}`}>
+          <div className="form-group">
+          <h3 className="title">Sygnio Sections</h3>
+          <div style={{ background: '#F18C16', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Dashboard</p>
+          </div>
+          {/* <div style={{ background: '#05DCC2', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Membership</p>
+          </div> */}
+          <div style={{ background: '#F0C734', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Staff Management</p>
+          </div>
+          <div style={{ background: '#E065E2', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Floor Management</p>
+          </div>
+          <div style={{ background: '#17D463', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Inventory Management</p>
+          </div>
+          <div style={{ background: '#1688F1', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>POS</p>
+          </div>
+          <div style={{ background: '#6C63FF', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Transactions</p>
+          </div>
+          <div style={{ background: '#355FCA', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Reports</p>
+          </div>
+          <div style={{ background: '#A64EFF', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Customers</p>
+          </div>
+          <div style={{ background: '#58B8AC', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Settings</p>
+          </div>
+          {/* <div style={{ background: '#16CAF1', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Gift cards</p>
+          </div> */}
+          <div style={{ background: '#4D77A7', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Orders</p>
+          </div>
+          {/* <div style={{ background: '#4D77A7', cursor: 'pointer' }} className="sideMenueWidgit text-center">
+            <i className="custom-icon icon-floor"></i>
+            <p>Deposits</p>
+          </div> */}
+        </div>
+        </div>
       </div>
-    )
-  } 
+    );
+  }
 }
 export default Sidebar;
